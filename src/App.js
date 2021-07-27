e9129340785b41883dda8e39ca599999e4c5cab8
aashish-07 <iaashish07@gmail.com>
2021-07-27 21:45:26 +0530

    filter-componene

--- src/App.js
+++ src/App.js
@@ -1,36 +1,80 @@
-import React from 'react';
+import React from "react";
 import './index.css'
-import data from './data.json'
-import Products from './components/Products';
-class App extends React.Component{
-    constructor(){
-      super();
-      this.state = {
-        products : data.products,
-        size: "",
-        sort: "",
-      };
+import data from "./data.json";
+import Products from "./components/Products";
+import Filter from "./components/Filter";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  sortProducts = (event) => {
+    // impl
+    const sort = event.target.value;
+    console.log(event.target.value);
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id < b._id
+            ? 1
+            : -1
+        ),
+    }));
+  };
+  filterProducts = (event) => {
+    // impl
+    console.log(event.target.value);
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, products: data.products });
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
     }
-    render(){
+  };
+  render() {
     return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React Shopping Cart</a>
-      </header>
-      <main>
-        <div className="content">
-          <Products products={this.state.products}></Products>
-        </div>
-        <div className="sidebar">
-            cart items
-        </div>
-      </main>
-      <footer>
-        ALL rights reserved
-      </footer>
-    </div>
-  );}
-  
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              ></Filter>
+              <Products products={this.state.products}></Products>
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All right is reserved.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
